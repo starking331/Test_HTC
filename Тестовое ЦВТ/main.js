f059309acf143d63cf6121fd6cb05c7e072d0fe7
starking331 <79844497+starking331@users.noreply.github.com>
2021-07-30 22:14:35 +0400

    )

---
+++ Тестовое ЦВТ/main.js
@@ -0,0 +1,18 @@
+$('.open-popup').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg').fadeIn(800);
+    $('html').addClass('no-scroll');
+});
+
+$('.close-popup').click(function() {
+    $('.popup-bg').fadeOut(800);
+    $('html').removeClass('no-scroll');
+});
+
+var form = document.getElementById('form');
+
+form.onsubmit = function() {
+   var variable = login.value;
+};
+
+
